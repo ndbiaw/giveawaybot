30ce94381b4be25533630ad62b0e6020bcaa9b86
立花 瀧 <59746573+ndbiaw@users.noreply.github.com>
2021-02-15 12:58:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+let fs = require('fs');
+const { prefix, token } = require('./config.json');
+
+client.on('ready', () => {
+  console.log(`Bot tag: ${client.user.tag}`);
+  console.log(`Guilds: ${client.guilds.cache.size}`);
+  client.user.setActivity(`${prefix}ga để tạo giveaway!`, { type: 'PLAYING' });
+});
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
+
+client.on('message', async message => {
+  if (message.content.startsWith(`${prefix}`)) {
+    let file_name = `${message.content.split(' ')[0].replace(prefix, '')}.js`;
+    if(!fs.existsSync('./commands/' + file_name)) return undefined;
+    if(fs.existsSync('./commands/' + file_name)) {
+      client.commands.get(file_name.replace('.js', '')).execute(client, message);
+    }
+  }
+});
+
+client.login(token);
